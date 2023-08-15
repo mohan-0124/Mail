d4f63af7a05ed0dc2b5830a1996f0ec576e51742
Mohan <142310159+mohan-0124@users.noreply.github.com>
2023-08-15 17:32:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const server = http.createServer((req, res) => {
   const urlReq = req.url;
   const parsedURL = url.parse(urlReq, true);
   const query = parsedURL.query;
-  console.log(query);
-  //   for (let key of query.keys()) {
-  //     console.log(`${key}: ${query[key]}`);
-  //   }
+  
   sendMail(
     query.userMail,
     query.password,
@@ -26,19 +23,11 @@ const server = http.createServer((req, res) => {
     : res.end("504 Some Techincal Issue ⚙");
 });
 
-function sendMail(userMail, password, name, email, subject, message) {
-  if (!name) {
-    console.log(`Enter name...`);
+function sendMail(userMail, password, _name, email, subject, message) {
+  if (!_name || !email)
     return false;
-  }
-  if (!email) {
-    console.log(`Enter email...`);
-    return false;
-  }
-  console.log(`Entered correct plzz wait...🤖`);
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    // port: 465,
     secure: true,
     auth: {
       user: userMail,
@@ -48,12 +37,11 @@ function sendMail(userMail, password, name, email, subject, message) {
   const mailOptions = {
     from: userMail,
     to: email,
-    subject: `From ${name}, ${subject}`,
+    subject: `From ${_name}, ${subject}`,
     text: message,
   };
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) console.log(err);
-    else console.log(`Mail sent: ${info.response}`);
   });
   return true;
 }
@@ -63,10 +51,3 @@ server.listen(PORT, (err) => {
   else console.log(`Server is running on port ${PORT}`);
 });
 
-/*//   console.log(`Url Req: ${urlReq}`);
-  //   console.log(parsedURL);
-  //   console.log("host: " + parsedURL.host);
-  //   console.log("path: " + parsedURL.pathname);
-  //   console.log("search: " + parsedURL.search);
-  //   console.log("\n\n\n\n\n\n\n");
-*/
